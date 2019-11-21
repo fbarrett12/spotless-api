94055cf9296743838c139893c177ac26c4bcac39
FredrickBarrett <f.barrett12@gmail.com>
2019-11-20 19:44:29 -0500

    used yelp api to seed data

--- Gemfile
+++ Gemfile
@@ -42,3 +42,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Adds serializers
 gem 'fast_jsonapi'
+
+# Adds HTTP gem
+gem "http"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "http"
+
+API_HOST = "https://api.yelp.com"
+SEARCH_PATH = "/v3/businesses/search"
+BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+API_KEY = ENV["YELP_API_KEY"]
+
+DEFAULT_BUSINESS_ID = "yelp-new-york-city"
+DEFAULT_TERM = "laundromat"
+DEFAULT_LOCATION = "New York, NY"
+SEARCH_LIMIT = 50
+
+def search(term, location)
+    url = "#{API_HOST}#{SEARCH_PATH}"
+    params = {
+      term: term,
+      location: location,
+      limit: SEARCH_LIMIT
+    }
+    response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
+    response.parse
+end
+
+laundromats = search("", "New York City")
+laundromats["businesses"].each do |laundromat|
+    if Provider.all.find_by(name: laundromat["name"])
+    else
+        Provider.create({
+            name: laundromat["name"],
+            # image: rest["image_url"],
+            address: laundromat["location"]["display_address"].join(" "),
+            # longitude: rest["coordinates"]["longitude"],
+            # latitude: rest["coordinates"]["latitude"],
+            # category: rest["categories"][0]["title"],
+            # borough: rest["location"]["city"]
+        })
+    end
+end
+
+# Restaurant.all.each do |rest| 
+#     3.times do 
+#         Dish.create({restaurant_id: rest.id, name: Faker::Food.dish})
+#     end 
+# end 
+puts "seeded"
